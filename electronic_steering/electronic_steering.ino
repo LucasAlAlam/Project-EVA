0ce3cce2bfed5742d78b741cd50dcd3d23c6e340
Lucas Al Alam <lucasalalamrs@gmail.com>
2019-11-26 00:00:43 -0300

    Engine and pcb testing

---
+++ electronic_steering/electronic_steering.ino
@@ -0,0 +1,22 @@
+/*
+ * This code reading AD values and apply to a linear transfer function to know the angle of a potentiometer
+*/
+int sensorPin = 15, , numRead, angle; 
+float sensorValue = 0, addRead = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  for (numRead = 0; numRead < 5000; numRead++)
+  {
+    sensorValue = analogRead(sensorPin);
+    addRead = addRead + sensorValue;
+  }
+  sensorValue = addRead / 5000;
+  angle = 0.06557377 * sensorValue - 122.7868852
+  Serial.println(angle);
+  delay(250);
+  somaLeituras = 0;
+}
